606626b1029fc7f8701f9b9c3c06b68e4b89e78c
Michael Abel <mabel@bitmuster.org>
2022-01-21 13:22:35 +0100

    Update testcases

--- test/test_pyst_process.py
+++ test/test_pyst_process.py
@@ -3,46 +3,46 @@ from syst_plugin.pyst_process import PystProcess
 
 
 def test_init():
-    pystp = PystProcess(22)
+    pystp = PystProcess(22, __file__)
     assert pystp.get_command() == 22
 
 
 def test_get_set_command():
-    pystp = PystProcess(33)
+    pystp = PystProcess(33, __file__)
     assert pystp.get_command() == 33
     pystp.set_command(77)
     assert pystp.get_command() == 77
 
 
 def test_kill_on_not_existing():
-    pystp = PystProcess(["notexist"])
+    pystp = PystProcess(["notexist"], __file__)
     with pytest.raises(SystemError):
         pystp.run_bg()
     assert pystp.kill() is None
 
 
 def test_set_name():
-    pystp = PystProcess(["theone"])
+    pystp = PystProcess(["theone"], __file__)
     pystp.set_name("one")
     assert pystp.get_name() == "one"
 
 
 def test_echo():
-    pystp = PystProcess(["echo", "hello", "world"])
+    pystp = PystProcess(["echo", "hello", "world"], __file__)
     pystp.run()
     assert pystp.get_stdout() == "hello world"
     assert pystp.get_returncode() == 0
 
 
 def test_true():
-    pystp = PystProcess("true")
+    pystp = PystProcess("true", __file__)
     pystp.run()
     assert pystp.get_stdout() == ""
     assert pystp.get_returncode() == 0
 
 
 def test_true_with_name():
-    pystp = PystProcess("true", name="Bob_")
+    pystp = PystProcess("true", __file__, name="Bob_")
     pystp.run()
     assert pystp.get_stdout() == ""
     assert pystp.get_returncode() == 0
@@ -51,7 +51,7 @@ def test_true_with_name():
 
 
 def test_false():
-    pystp = PystProcess("false")
+    pystp = PystProcess("false", __file__)
     pystp.run()
     assert pystp.get_stdout() == ""
     assert pystp.get_stderr() == ""
@@ -60,7 +60,7 @@ def test_false():
 
 def test_stderr():
     exp = "ls: cannot access 'notthere': No such file or directory"
-    pystp = PystProcess(["ls", "notthere"])
+    pystp = PystProcess(["ls", "notthere"], __file__)
     pystp.run()
     assert pystp.get_stdout() == ""
     assert pystp.get_stderr() == exp
@@ -69,13 +69,13 @@ def test_stderr():
 
 def test_stderr_file():
     exp = "ls: cannot access 'notthere': No such file or directory"
-    pystp = PystProcess(["ls", "notthere"])
+    pystp = PystProcess(["ls", "notthere"], __file__)
     pystp.run()
     assert pystp.get_stderr() == exp
 
 
 def test_stdout_file():
     exp = "hello nice ball"
-    pystp = PystProcess(["echo", "hello", "nice", "ball"])
+    pystp = PystProcess(["echo", "hello", "nice", "ball"], __file__)
     pystp.run()
     assert pystp.get_stdout() == exp
