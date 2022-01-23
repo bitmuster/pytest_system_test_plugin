1aa5bab65e03eb40148eeb97ebe1a6829884f6bb
Michael Abel <mabel@bitmuster.org>
2022-01-23 23:49:06 +0100

    Add disabled testcases and todo

--- test/test_scopes.py
+++ test/test_scopes.py
@@ -1,5 +1,22 @@
 import logging
 
+# TODO Fix disabled test cases: one of them makes the plugin leak
+#       processes or disturbs the execution order
+
+def dis_test_proc_factory_module_2(process_factory_module ):
+    logging.info("That one runs without the module fixture")
+    p = process_factory_module("/usr/bin/true")
+    p.run_bg()
+
+def dis_test_proc_factory_module(process_factory_module):
+    logging.info("here we create the fixture")
+    p=process_factory_module("/usr/bin/true")
+    p.run_bg()
+
+def dis_test_proc_factory_module_2(process_factory_module ):
+    logging.info("here we create the fixture 2")
+    p=process_factory_module("/usr/bin/true")
+    p.run_bg()
 
 def test_proc_factory_function(process_factory):
     logging.info("here we run a process")
@@ -9,5 +26,15 @@ def test_proc_factory_function(process_factory):
 def test_proc_factory_function_2(process_factory):
     logging.info("here we run a process 2")
     p=process_factory("/usr/bin/true")
+    p.run_bg()
+
+def dis_test_proc_factory_session(process_factory_session):
+    logging.info("here we run a session fixture")
+    p=process_factory_session("/usr/bin/true")
+    p.run_bg()
+
+def dis_test_proc_factory_function_2(process_factory_session):
+    logging.info("here we run a session fixture 2")
+    p=process_factory_session("/usr/bin/true")
     p.run_bg()
 
