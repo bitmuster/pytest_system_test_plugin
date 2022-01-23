6ead533997163548bdf94eab68df162e758a308b
Michael Abel <mabel@bitmuster.org>
2022-01-23 23:41:58 +0100

    Add session and module scope

--- syst_plugin/plugin.py
+++ syst_plugin/plugin.py
@@ -9,6 +9,7 @@ from . import PystProcess
 
 # logging.basicConfig(level=logging.DEBUG)
 
+# TODO: Automatically delete output folders
 
 
 @pytest.fixture(scope='function')
@@ -49,3 +50,60 @@ def process_factory(request):
     for proc in processes:
         logging.info("Killing process %s", proc.child)
         proc.kill()
+
+
+@pytest.fixture(scope="module")
+def process_factory_module(request):
+    """process base fixture factory
+    TODO: Avoid duplication of code
+    TODO: Test this with pytester
+    """
+
+    processes = []
+
+    def _make_process(cmd, name=None):
+        if not name:
+            name = secrets.token_hex(2) + "_"
+        logging.info("    >>>>>>>> We generate a module scope process with name %s", name)
+        proc = PystProcess(
+            command=cmd, logpath=request.fspath, testname=request.node.name, name=name
+        )
+        processes.append(proc)
+        return proc
+
+    yield _make_process
+
+    # TODO: Do we kill processes in the right order?
+    # TODO: Wo we kill them right?
+    for proc in processes:
+        logging.info("    <<<<<<<< We kill a module scope generated process")
+        logging.info("Killing process %s", proc.child)
+        proc.kill()
+
+@pytest.fixture(scope="session")
+def process_factory_session(request):
+    """process base fixture factory
+    TODO: Avoid duplication of code
+    TODO: Test this with pytester
+    """
+    processes = []
+
+    def _make_process(cmd, name=None):
+        if not name:
+            name = secrets.token_hex(2) + "_"
+        logging.info("    >>>>>>>> We generate a session scope process with name %s", name)
+        proc = PystProcess(
+            command=cmd, logpath="session_", testname=request.node.name, name=name
+        )
+        processes.append(proc)
+        return proc
+
+    yield _make_process
+
+    # TODO: Do we kill processes in the right order?
+    # TODO: Wo we kill them right?
+    for proc in processes:
+        logging.info("    <<<<<<<< We kill a session scope generated process")
+        logging.info("Killing process %s", proc.child)
+        proc.kill()
+
