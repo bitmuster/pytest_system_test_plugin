58086a32d47824a54aad5a33376f610ea0fe19d5
Michael Abel <mabel@bitmuster.org>
2022-01-24 00:06:58 +0100

    Update tox

--- test/test_scopes.py
+++ test/test_scopes.py
@@ -8,32 +8,32 @@ def test_proc_factory_module_first(process_factory):
     logging.info(
         "That one runs without the module fixture (no module fixture requested yet)"
     )
-    p = process_factory("/usr/bin/true")
-    p.run_bg()
+    proc = process_factory("/usr/bin/true")
+    proc.run_bg()
 
 
 def test_proc_factory_module(process_factory_module):
     logging.info("here we create the fixture")
-    p = process_factory_module("/usr/bin/true")
-    p.run_bg()
+    proc = process_factory_module("/usr/bin/true")
+    proc.run_bg()
 
 
 def test_proc_factory_module_2(process_factory_module):
     logging.info("here we create the fixture 2")
-    p = process_factory_module("/usr/bin/true")
-    p.run_bg()
+    proc = process_factory_module("/usr/bin/true")
+    proc.run_bg()
 
 
 def test_proc_factory_function(process_factory):
     logging.info("here we run a process")
-    p = process_factory("/usr/bin/true")
-    p.run_bg()
+    proc = process_factory("/usr/bin/true")
+    proc.run_bg()
 
 
 def test_proc_factory_function_2(process_factory):
     logging.info("here we run a process 2")
-    p = process_factory("/usr/bin/true")
-    p.run_bg()
+    proc = process_factory("/usr/bin/true")
+    proc.run_bg()
 
 
 def dis_test_proc_factory_session(process_factory_session):
@@ -41,11 +41,11 @@ def dis_test_proc_factory_session(process_factory_session):
     Do we have a os.fork going wild?
     """
     logging.info("here we run a session fixture")
-    p = process_factory_session("/usr/bin/true")
-    p.run_bg()
+    proc = process_factory_session("/usr/bin/true")
+    proc.run_bg()
 
 
 def dis_test_proc_factory_function_2(process_factory_session):
     logging.info("here we run a session fixture 2")
-    p = process_factory_session("/usr/bin/true")
-    p.run_bg()
+    proc = process_factory_session("/usr/bin/true")
+    proc.run_bg()
