6f65c7611018fb383da476504312bd5b43642f41
Michael Abel <mabel@bitmuster.org>
2022-01-24 00:02:51 +0100

    Enable tests for module scope

--- test/test_scopes.py
+++ test/test_scopes.py
@@ -6,19 +6,19 @@ import logging
 
 def test_proc_factory_module_first(process_factory):
     logging.info(
-        "That one runs without the module fixture (no module fixture" + "requested yet)"
+        "That one runs without the module fixture (no module fixture requested yet)"
     )
     p = process_factory("/usr/bin/true")
     p.run_bg()
 
 
-def dis_test_proc_factory_module(process_factory_module):
+def test_proc_factory_module(process_factory_module):
     logging.info("here we create the fixture")
     p = process_factory_module("/usr/bin/true")
     p.run_bg()
 
 
-def dis_test_proc_factory_module_2(process_factory_module):
+def test_proc_factory_module_2(process_factory_module):
     logging.info("here we create the fixture 2")
     p = process_factory_module("/usr/bin/true")
     p.run_bg()
