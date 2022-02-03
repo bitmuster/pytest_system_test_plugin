dd77944e1230dfc77553c37d0d92ebad02da455c
Michael Abel <mabel@bitmuster.org>
2022-02-03 23:46:21 +0100

    Update setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="pytest_system_test_plugin",
-    version="0.0.1",
+    version="0.0.2",
     packages=["syst_plugin"],
+    author="Michael Abel",
+    author_email="python@bitmuster.org",
+    description="Pyst - Pytest System-Test Plugin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/bitmuster/pytest_system_test_plugin",
+    project_urls={
+        "GitLab": "https://gitlab.com/bitmuster/pytest_system_test_plugin",
+        "GitHub": "https://github.com/bitmuster/pytest_system_test_plugin",
+        "Examples": "https://github.com/abelikt/pytest_system_test_plugin_test"
+    },
     entry_points={"pytest11": ["pytest_system_test_plugin = syst_plugin.plugin"]},
     classifiers=["Framework :: Pytest"],
 )
